0b688ab5266053d31f15e3ec7a0e90451bab0588
Adrien Szmul <aszmul@excilys.com>
2018-03-20 17:57:58 +0100

    [SERVLET] Started servlet

--- src/main/java/com/excilys/formation/computerdatabase/servlets/Test.java
+++ src/main/java/com/excilys/formation/computerdatabase/servlets/Test.java
@@ -1,7 +1,9 @@
 package com.excilys.formation.computerdatabase.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
